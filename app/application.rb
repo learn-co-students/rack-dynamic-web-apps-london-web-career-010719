518f0905646e39dfb522b2360bc94a544b3054a3
studioshane <studioshane@gmail.com>
2019-01-26 15:49:25 +0000

    game working

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,13 @@ class Application
     resp = Rack::Response.new
     
     
-    num1 = Kernel.new(1..20)
-    num2 = Kernel.new(1..20)
-    num3 = Kernel.new(1..20) 
+    num1 = Kernel.rand(1..2)
+    num2 = Kernel.rand(1..2)
+    num3 = Kernel.rand(1..2) 
+
+    resp.write "#{num1} \n" 
+    resp.write "#{num2} \n" 
+    resp.write "#{num3} \n" 
     
     if num1==num2 && num2==num3
       resp.write "You Win"
@@ -18,3 +22,25 @@ class Application
   end
 
 end
+
+
+# class Application
+ 
+#   def call(env)
+#     resp = Rack::Response.new
+ 
+#     num_1 = Kernel.rand(1..20)
+#     num_2 = Kernel.rand(1..20)
+#     num_3 = Kernel.rand(1..20)
+ 
+#     if num_1==num_2 && num_2==num_3
+#       resp.write "You Win"
+#     else
+#       resp.write "You Lose"
+#     end
+ 
+#     resp.finish
+#   end
+ 
+# end
+
